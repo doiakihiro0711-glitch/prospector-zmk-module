f07a7eef0534def71c7fe14a840936efe0a8dedb
doiakihiro0711-glitch <doiakihiro0711@gmail.com>
2026-08-02 11:48:52 +0900

    Update status_screen.c

--- boards/shields/prospector_adapter/src/layouts/operator/status_screen.c
+++ boards/shields/prospector_adapter/src/layouts/operator/status_screen.c
@@ -29,7 +29,7 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_pos(zmk_widget_layer_display_obj(&layer_display_widget), 10, 142);
 
     zmk_widget_battery_circles_init(&battery_circles_widget, screen);
-    lv_obj_set_pos(zmk_widget_battery_circles_obj(&battery_circles_widget), 74, 170);
+    lv_obj_set_pos(zmk_widget_battery_circles_obj(&battery_circles_widget), 41, 170);
 
     zmk_widget_output_init(&output_widget, screen);
     lv_obj_set_pos(zmk_widget_output_obj(&output_widget), 148, 170);
